a661813381c18861ec16e59c86f17472dc42f3a5
ktgitfrog333 <paraysis+git@gmail.com>
2023-12-29 10:31:16 +0900

    no message

--- adminpage/vite.config.ts
+++ adminpage/vite.config.ts
@@ -12,5 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/adminpage/dist/' // This line has been added
 })
